e24277c7dff074a658a934205545fbc1795c4fe3
zmwilliam <willcxa@gmail.com>
2021-12-18 17:54:25 -0300

    Lua config: options, keymaps, packer

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,40 @@
+local options = {
+  backup = false,                          -- don't creates a backup file
+  clipboard = "unnamedplus",                -- allows neovim to access the syst em clipbard
+  cmdheight = 2,                           -- more space to display messages at the command line
+  completeopt = {'menu', 'menuone', 'noselect'}, -- cmp requirement
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  cursorline = true,                       -- highlight the current line
+  expandtab = true,                        -- convert tabs to spaces
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  number = true,                           -- set numbered lines
+  numberwidth = 2,                         -- set number column width to 2 {default 4}
+  relativenumber = true,                  -- set relative numbered lines
+  scrolloff = 8,                           -- always show N lines above and bellow the cursor
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  showtabline = 2,                         -- always show tabs
+  sidescrolloff = 8,
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- don't creates a swapfile
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  wrap = false,                            -- display lines as one long line
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
